d2dae66582a1b4ccfeb1bc7ab992d5dba0ee0521
oamca <64345120+oamca@users.noreply.github.com>
2022-06-20 15:21:52 +0800

    Create ok.sh

---
+++ ok.sh
@@ -0,0 +1,113 @@
+apt update
+apt install curl nano ufw -y
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
+rm /usr/local/etc/xray/config.json
+#nano /usr/local/etc/xray/config.json
+cat << EOF > /usr/local/etc/xray/config.json
+{
+    "log": {
+        "loglevel": "none",
+      "access":"access.log",
+      "error":"error.log"
+    },
+    "inbounds": [
+        {
+            "port":80,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "54679b8c-6d07-4f49-97b7-538ebce2a812", 
+                        "level": 1,
+                        "email": "noreply@gmail.com",
+                        "alterId":0
+                    }
+                ],
+                "disableInsecureEncryption": true,
+                "decryption": "auto"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "/fastssh" 
+                }                
+            }
+        },
+      {
+         "port":8080,
+         "protocol":"vmess",
+         "settings":{
+            "clients":[
+               {
+                  "id":"54679b8c-6d07-4f49-97b7-538ebce2a812",
+                  "level":1,
+                  "email":"noreply@gmail.com",
+                  "alterId":0
+               }
+            ],
+            "disableInsecureEncryption":true,
+            "decryption":"auto"
+         },
+         "streamSettings":{
+            "network":"tcp",
+            "security":"none",
+            "tcpSettings":{
+               "acceptProxyProtocol":false,
+               "header":{
+                  "type":"http",
+                  "response":{
+                     "version":"1.1",
+                     "status":"200",
+                     "reason":"OK",
+                     "headers":{
+                        "Content-Type":[
+                           "application/octet-stream",
+                           "video/mpeg",
+                           "text/html"
+                        ],
+                        "Transfer-Encoding":[
+                           "chunked"
+                        ],
+                        "Connection":[
+                           "keep-alive"
+                        ],
+                        "Pragma":"no-cache"
+                     }
+                  }
+               }
+            }
+         }
+      }
+    ],
+  "outbounds": [
+    {
+      "tag": "direct",
+      "protocol": "freedom",
+      "settings": {}
+    },    
+    {
+      "tag": "blocked",
+      "protocol": "blackhole",
+      "settings": {}
+    }
+  ]
+}
+EOF
+ufw allow ssh
+ufw allow from 192.168.1.1
+ufw enable
+cat << EOF > /usr/bin/s.sh
+ufw status numbered
+echo "begin with 1: "
+read numdel
+ufw delete ${numdel}
+myip="${SSH_CLIENT%% *}"
+ufw allow from ${myip}
+ufw status verbose
+EOF
+chmod +x /usr/bin/s.sh
+service xray restart
